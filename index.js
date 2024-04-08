6673934eca1d72c75cc7b126b5fe0b487c384983
nimesh-korat <nimeshkorat611@gmail.com>
2024-04-08 11:15:35 +0530

    cors

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ const PORTS = 8001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use('/images/productPics', express.static('images/productPics'));
 app.use('/images/profilePics', express.static('images/profilePics'));
 
