990e55cc2073f98438a510eb6f15921170711d49
nimesh-korat <nimeshkorat611@gmail.com>
2024-04-08 11:28:11 +0530

    cors

--- index.js
+++ index.js
@@ -24,14 +24,11 @@ const PORTS = 8001;
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use('/images/productPics', express.static('images/productPics'));
 app.use('/images/profilePics', express.static('images/profilePics'));
 
