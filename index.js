651b9a3f84911dc707732299c91842e286af003f
nimesh-korat <nimeshkorat611@gmail.com>
2024-04-08 16:34:10 +0530

    session

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(session({
         autoRemoveInterval: 1440
     }),
     cookie: {
-        secure: false, // Use secure cookies for HTTPS
+        secure: true, // Use secure cookies for HTTPS
     }
 }));
 
