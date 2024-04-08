0383543a2eb9aefb2af8be53c621db7bbf6a51eb
nimesh-korat <nimeshkorat611@gmail.com>
2024-04-08 11:49:58 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const { GetProduct } = require("./apis/getProduct");
 const { EditProduct } = require("./apis/editProduct");
 const { DeleteProduct } = require("./apis/deleteProduct");
 const { FetchAllProducts } = require("./apis/fetchAllProducts");
+const MongoStore = require('connect-mongo');
 require('dotenv').config();
 //initialize app
 const app = express();
@@ -39,7 +40,11 @@ app.use('/images/profilePics', express.static('images/profilePics'));
 app.use(session({
     secret: 'your-secret-key',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: new MongoStore({ 
+        mongoUrl: process.env.MONGO_URL,
+        autoRemove: 'native' // Optional. Automatically remove expired sessions.
+    })
 }));
 
 //signup API
