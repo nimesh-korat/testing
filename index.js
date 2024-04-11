556a3fd17f95d35c0affbfaf27d08f493e62bd1b
nimesh-korat <nimeshkorat611@gmail.com>
2024-04-11 11:58:33 +0530

    session

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(session({
     cookie: {
         secure: true,
         sameSite: 'none',
-        domain: '*.onrender.com',
+        domain: '.onrender.com',
         maxAge: 1000 * 60 * 60 * 24 //oneDay
     }
 }));
